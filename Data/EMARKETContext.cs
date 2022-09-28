ce02f3fc398e60061f4a04d74347ecb7f385b4c1
cetindag2 <112568888+cetindag2@users.noreply.github.com>
2022-09-28 22:38:04 +0300

    Update EMARKETContext.cs

--- Data/EMARKETContext.cs
+++ Data/EMARKETContext.cs
@@ -26,8 +26,11 @@ namespace EMARKET_MVC.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=Localhost\\SQLEXPRESS;Database=EMARKET;User ID=cetin;Password=1234");
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("MSSQL");
             }
         }
 
